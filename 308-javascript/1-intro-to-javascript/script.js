236227774e8ffcc29412932b9f3003a91d8e963f
sramirez124 <forwardemails124@gmail.com>
2024-09-09 11:22:29 -0500

    Java folder added

---
+++ 308-javascript/1-intro-to-javascript/script.js
@@ -0,0 +1,19 @@
+// Intro to JavaScript
+
+/**
+ * This is a
+ * multi-line comment
+ */
+
+console.log("Hello World!!")
+
+// Different data types
+
+//Number
+4
+1.1
+100
+
+// String
+
+"Hello to String"
