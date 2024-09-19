90cbf4f4d626a33ed0e3c9e6c7cd2af9a53726e1
sramirez124 <forwardemails124@gmail.com>
2024-09-19 14:13:00 -0500

    added rest of the code

--- 308-javascript/5-functions/script.js
+++ 308-javascript/5-functions/script.js
@@ -1,21 +1,21 @@
-// console.log('Functions');
+console.log('Functions');
 // Verbs of our code
 
 // Three ways to define a function
 
 //1. Function Declaration
 function sayHello() {
-  // console.log('hello world!');
+  console.log('hello world!');
 }
 
 //2 Function Expression
 const sayHi = function () {
-  // console.log('hi!');
+  console.log('hi!');
 };
 
 //3. Arrow Function ES6
 const sayGoodMorning = () => {
-  // console.log('Good morning!');
+  console.log('Good morning!');
   return 'Good morning!';
 };
 
@@ -35,14 +35,14 @@ fnDeclaration(); // thank you function declarations :)
 
 // this function gets hoisted
 function fnDeclaration() {
-  // console.log("I'm coming from a function declaration.");
+  console.log("I'm coming from a function declaration.");
 }
 
 // does NOT get hoisted
 const fnExpression = function () {
-  // console.log("I'm coming from a function expression.");
+  console.log("I'm coming from a function expression.");
 };
-// // console.log(fnExpression);
+// console.log(fnExpression);
 // fnExpression(); // works just fine, because we are referenceing after it's been defined.
 //========================================================
 
@@ -66,7 +66,7 @@ function areBothEven(n1, n2) {
   return !(n1 % 2) && !(n2 % 2);
 }
 
-// console.log(areBothEven(8, 6));
+console.log(areBothEven(8, 6));
 
 // Practice Activity 1: Write a Function Declaration
 function computeArea(width, height) {
@@ -75,7 +75,7 @@ function computeArea(width, height) {
   return s;
 }
 
-// console.log(computeArea(5, 10));
+console.log(computeArea(5, 10));
 
 // Extra
 //========================================================
@@ -99,7 +99,7 @@ for (let i = 0; i < squares.length; i++) {
   const area = computeArea(squares[i].width, squares[i].height);
   squareAreas.push(area);
 }
-// // console.log(squareAreas);
+// console.log(squareAreas);
 //========================================================
 
 // Practice Activity 2: Write a Function Expression
@@ -115,15 +115,15 @@ const planetHasWater = function (planet) {
   return lowercasePlanet === 'earth' || lowercasePlanet === 'mars';
 };
 
-// console.log(planetHasWater(9));
+console.log(planetHasWater(9));
 
-// console.log(planetHasWater('EaRtH'));
+console.log(planetHasWater('EaRtH'));
 
 // Parameters and Arguments
 // arguments give the parameter their value
 
 function foo(a, b, c) {
-  // console.log(a, b, c);
+  console.log(a, b, c);
 }
 
 foo(18, 20);
@@ -151,7 +151,7 @@ const devObject = getDevObject(
   'GraphQL'
 );
 
-// // console.log(devObject);
+// console.log(devObject);
 
 function getDevObject2(name, ...skills) {
   return {
@@ -171,9 +171,13 @@ const devObject2 = getDevObject2(
   'GraphQL'
 );
 
-// // console.log(devObject2);
+// console.log(devObject2);
 
-// Default Params
+/**
+ * Default Parameters
+ *
+ *
+ */
 
 const bike = {
   color: 'red',
@@ -184,7 +188,7 @@ const bike = {
 function setColor(bicycle, color) {
   bicycle.color = color || 'purple';
 
-  // console.log(bicycle);
+  console.log(bicycle);
 }
 
 // setColor(bike, 'green');
@@ -192,41 +196,164 @@ function setColor(bicycle, color) {
 function setColorES6(bicycle, color = 'purple') {
   bicycle.color = color;
 
-  // console.log(bicycle);
+  console.log(bicycle);
 }
 
-setColorES6(bike, 'yellow');
+// setColorES6(bike);
 
-/**
- * 
- *  Functions as Arguments
- * 
- */
+//Functions as Arguments
+let colors = ['red', 'green', 'blue'];
 
-let colors = ["red", "green", "blue"];
-// function logColor(color){
-//     console.log(color)
-// }
+function logColor(color) {
+  console.log(color);
+}
 
-// Passing an anonymous function to the forEach method
-colors.forEach((colors) => {
-    // console.log(colors)
+// logColor('green', 'red');
+
+// colors.forEach(logColor);
+
+// passing an anonymous function to the forEach method
+colors.forEach((color) => {
+  console.log(color);
 });
 
-/**
- * 
- * Immediately Invoked Function Expressions (IIFE)
- * 
- */
-
+//IIFE
 (function () {
-    console.log("I'm running without calling the function");
+  var a = 'a';
+  // console.log('im running without calling the function');
 })();
 
+// console.log(a); // throws a referance error
 
 /**
- * 
+ *
  * Hoisting
- * 
+ *
+ */
+// Three Types!
+// Value Hoisting (function declarations)
+// 1 .Being able to use a variable's value in its scope before the line it is declared. ("Value hoisting")
+
+// Declaration Hoisting
+// 2. Being able to reference a variable in its scope before the line it is declared, without throwing a ReferenceError, but the value is always undefined. ("Declaration hoisting")
+
+// 3. The declaration of the variable causes behavior changes in its scope before the line in which it is declared.
+
+function hoist() {
+  // Type 2
+  console.log(x); // undefined, not a reference
+  var x = 25;
+  console.log(x); // 25
+}
+
+hoist();
+
+// Type 3 Behavior
+
+let x = 5;
+
+// block is here to establish anohter scope.
+{
+  // when it comes to let and const
+  // The JS engine will ALWAYS try to retrieve the variable within that same scope, even if the variable is defined later on in the scope
+
+  // the JS engine thinks your trying to access the x defined within this scope.
+  // it assumes that the same-scope x is what you meant to use, but since it can't reference that value yet... ReferenceError.
+  // console.log(x); // throws a Reference error
+
+  let x = 6;
+}
+
+/**
+ *
+ * Nesting Functions
+ *
+ */
+function openNewAccount(name, openingBalance) {
+  let acctNum = generateAcctNum();
+
+  // createAccount is a function available outside this function
+  let acct = createAccount(acctNum, openingBalance);
+  return acct;
+
+  // generateAcctNum function in there and it's only relevant to when a new account is opened, so it's nested within the openNewAcount function.
+  // helper function that provides a unique account number
+  function generateAcctNum() {
+    return Date.now(); // super amazing algorithm :)
+  }
+}
+
+/**
+ *
+ * Pass By Value
+ * Only for Primitive types (string, number, boolean)
  */
 
+let myVar = 20;
+
+function passBy(myVar) {
+  myVar = myVar * 10;
+  return myVar;
+}
+
+console.log(myVar); // 20
+console.log(passBy(myVar)); // 200
+console.log(myVar); // 20
+
+let z = 10;
+
+let h = 15;
+
+let m = h;
+
+m += 5;
+console.log('m: ', m, 'h: ', h);
+
+/**
+ *
+ * Pass By Reference
+ * Only for Reference types (arrays, objects, functions)
+ */
+
+let myVar2 = { value: 20, type: 'Number' };
+
+function passBy2(x) {
+  x.value = x.value * 20;
+}
+
+console.log(myVar2); // 20
+passBy2(myVar2); // 400
+console.log(myVar2); // 400
+
+let arr = ['hi', 'hello', 'bonjour'];
+
+let another = arr;
+
+another.push('arr matey');
+
+//arr and another are pointing to the same place in memory due to the array being a reference type
+console.log(arr);
+
+console.log(another);
+
+// console.log([] === []); // false, arrays carry different references
+
+console.log(arr === another);
+
+//=========================================
+let numArray = [10, 20, 0, -10, 15, 28, -1000, 42, 7];
+
+function returnEven(array) {
+  for (let i in array) {
+    if (array[i] % 2 !== 0) {
+      array.splice(i, 1);
+    }
+  }
+
+  return array;
+}
+
+console.log(numArray); // (9) [10, 20, 0, -10, 15, 28, -1000, 42, 7]
+let newArray = returnEven(numArray);
+console.log(newArray); // (7) [10, 20, 0, -10, 28, -1000, 42]
+console.log(numArray); // (7) [10, 20, 0, -10, 28, -1000, 42]
